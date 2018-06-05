10b6351bad37fc21b6b1426d9a680469e99bc2a4
polon59 <przemyslaw.polczak@gmail.com>
2018-06-05 16:40:17 +0200

    create class 'BubbleSort.java' with implemented simple sorting algorithm

---
+++ BubbleSort.java
@@ -0,0 +1,29 @@
+/*
+*This is simple implementation of bubblesort algorithm
+*/
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] array = {3,5,6,11,4,15,143,333,7,8,88,2};
+        bubbleSort.handleSort(array);
+    }
+
+
+    public void handleSort(int[] array){
+        int temporary = 0;
+
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j <array.length-i-1; j++){
+                if (array[j+1] < array[j]){
+                    temporary = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temporary;
+                }
+            }
+        }
+        for (int a : array){
+            System.out.print(a + " ");
+        }
+    }
+}
